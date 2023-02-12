ccbb4e895b9bee4eb97661cc65efc30e31c1bc96
YuriShepel <donbidonn@gmail.com>
2023-02-12 16:50:29 +0200

    DataBase changed, env. added

---
+++ oracul_site/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for oracul_site project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oracul_site.settings')
+
+application = get_wsgi_application()
