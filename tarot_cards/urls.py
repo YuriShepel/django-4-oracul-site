14a42d466351714dae75fd9df64c0b228c2400cb
YuriShepel <donbidonn@gmail.com>
2023-03-07 14:32:58 +0200

    caching for tarot_cards urls implemented

--- tarot_cards/urls.py
+++ tarot_cards/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-
+from django.views.decorators.cache import cache_page
 from . import views
 
 app_name = 'tarot_cards'
 
 urlpatterns = [
-    path('tarot_suits/', views.TarotSuitListView.as_view(), name='tarot_suits'),
-    path('tarot_suits/<slug:slug>/', views.CardsListView.as_view(), name='cards_list'),
-    path('suit_description/<slug:slug>/', views.TarotSuitDetailView.as_view(), name='suit_description'),
-    path('<slug:slug>/', views.CardDetailView.as_view(), name='card_detail'),
+    path('tarot_suits/', cache_page(30)(views.TarotSuitListView.as_view()), name='tarot_suits'),
+    path('tarot_suits/<slug:slug>/', cache_page(30)(views.CardsListView.as_view()), name='cards_list'),
+    path('suit_description/<slug:slug>/', cache_page(30)(views.TarotSuitDetailView.as_view()), name='suit_description'),
+    path('<slug:slug>/', cache_page(30)(views.CardDetailView.as_view()), name='card_detail'),
 ]
