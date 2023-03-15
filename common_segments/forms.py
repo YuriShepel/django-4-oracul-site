8bdc1f83b989fb0ce98374556d58033bf67c1dfa
YuriShepel <donbidonn@gmail.com>
2023-03-15 14:14:17 +0200

    feature: celery installed and configured

---
+++ common_segments/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Feedback
+
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['name', 'email', 'message']
+        labels = {
+            'name': 'Ваше имя:',
+            'email': 'Ваш Email:',
+            'message': 'Введите сообщение:',
+        }
+        widgets = {
+            'message': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'id': 'exampleFormControlTextarea1'}),
+            'name': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Имя', 'id': 'exampleFormControlInput1'}),
+            'email': forms.EmailInput(
+                attrs={'class': 'form-control', 'placeholder': 'name@example.com', 'id': 'exampleFormControlInput2'}),
+        }
